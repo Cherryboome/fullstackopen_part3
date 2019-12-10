3cc2c1fbb391e25e3b2401bda7e3cc72468ccb2b
Cherryboome <muhamidifarian@yahoo.com>
2019-12-10 15:21:32 +0200

    completed exercise 3.2: Phonebook backend step2

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ let persons = [
   }
 ];
 
+const personsNum = () => {
+  if (persons.length === 1) {
+    return `${persons.length} person`;
+  } else {
+    return `${persons.length} people`;
+  }
+};
+
 app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
@@ -32,4 +40,10 @@ const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+app.get("/info", (req, res) => {
+  const date = new Date();
+  res.send(`<div><p>Phonebook has info for ${personsNum()}</p> 
+  <p>${date}</p></div>`);
 });
