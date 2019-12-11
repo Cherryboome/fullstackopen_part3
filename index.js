f248a13d6347368ecaff8345fe585c0d2749ef93
Cherryboome <muhamidifarian@yahoo.com>
2019-12-11 21:45:05 +0200

    configured backend with heroku

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 
 const app = express();
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 app.use(bodyParser.json());
 
@@ -34,6 +35,8 @@ app.use(
     }
   })
 );
+
+app.use(cors());
 
 let persons = [
   {
@@ -134,7 +137,7 @@ app.post("/api/persons", (req, res) => {
   res.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
