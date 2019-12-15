587ee05d0164fd0479b555731d25e9c55c1e89df
Cherryboome <muhamidifarian@yahoo.com>
2019-12-15 12:14:08 +0200

    completed exercises 3.13: Phonebook database, step1 & 3.14: Phonebook database, step2

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+
+const Person = require("./models/person");
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -37,28 +40,28 @@ app.use(
   })
 );
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123-4567",
-    id: 1
-  },
-  {
-    name: "Ada Lovelace",
-    number: "040-345-0987",
-    id: 2
-  },
-  {
-    name: "Dan Abramov",
-    number: "040-213-7648",
-    id: 3
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "040-746-0399",
-    id: 4
-  }
-];
+// let persons = [
+//   {
+//     name: "Arto Hellas",
+//     number: "040-123-4567",
+//     id: 1
+//   },
+//   {
+//     name: "Ada Lovelace",
+//     number: "040-345-0987",
+//     id: 2
+//   },
+//   {
+//     name: "Dan Abramov",
+//     number: "040-213-7648",
+//     id: 3
+//   },
+//   {
+//     name: "Mary Poppendieck",
+//     number: "040-746-0399",
+//     id: 4
+//   }
+// ];
 
 app.use(express.static("build"));
 
@@ -71,18 +74,23 @@ const personsNum = () => {
 };
 
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then(persons => {
+    res.json(persons.map(person => person.toJSON()));
+  });
 });
 
 app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find(person => person.id === id);
+  Person.findById(req.params.id).then(person => {
+    res.json(person.toJSON());
+  });
+  // const id = Number(req.params.id);
+  // const person = persons.find(person => person.id === id);
 
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
+  // if (person) {
+  //   res.json(person);
+  // } else {
+  //   res.status(404).end();
+  // }
 });
 
 app.get("/api/info", (req, res) => {
@@ -98,47 +106,53 @@ app.delete("/api/persons/:id", (req, res) => {
   res.status(204).end();
 });
 
-const generateId = () => {
-  const id = persons.length > 0 ? Math.round(Math.random() * 1000) : 0;
-  const personId = persons.find(person => person.id === id);
+// const generateId = () => {
+//   const id = persons.length > 0 ? Math.round(Math.random() * 1000) : 0;
+//   const personId = persons.find(person => person.id === id);
 
-  if (personId) {
-    return generateId();
-  } else {
-    return id;
-  }
-};
+//   if (personId) {
+//     return generateId();
+//   } else {
+//     return id;
+//   }
+// };
 
 app.post("/api/persons", (req, res) => {
   const body = req.body;
-  const existingPerson = persons.find(
-    person => person.name.toLowerCase() === body.name.toLowerCase()
-  );
 
-  if (!body.name) {
+  // const existingPerson = persons.find(
+  //   person => person.name.toLowerCase() === body.name.toLowerCase()
+  // );
+
+  if (body.name === undefined) {
     return res.status(400).json({
       error: "name missing"
     });
-  } else if (!body.number) {
+  } else if (body.number === undefined) {
     return res.status(400).json({
       error: "number missing"
     });
-  } else if (existingPerson) {
-    return res.status(409).json({ error: "name must be unique" });
   }
+  // else if (existingPerson) {
+  //   return res.status(409).json({ error: "name must be unique" });
+  // }
 
-  const person = {
+  const person = new Person({
     name: body.name,
-    number: body.number,
-    id: generateId()
-  };
+    number: body.number
+    // id: generateId()
+  });
 
-  persons = persons.concat(person);
+  person.save().then(savedPerson => {
+    res.json(savedPerson.toJSON());
+  });
 
-  res.json(person);
+  // persons = persons.concat(person);
+
+  // res.json(person);
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
